3cc159ae97f55e22615408e423499cd7ce37685a
Robot8lover <108429990+Robot8lover@users.noreply.github.com>
2024-08-03 16:11:42 -0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,15 +1,34 @@
 import express from "express";
 import logger from "morgan";
-import io from "io";
+import path from "path";
+
+import http from "http";
+import debugModule from "debug";
+const debug = debugModule("fish-app:server");
+
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+import io from "./io.js";
 
 const app = express();
 const port = 3000;
 
-io.on("connection", (socket) => {
-})
+debug.enabled = true;
+
+io.on('connection', (socket) => {
+  debug('a user connected');
+
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', (data) => {
+    debug(data);
+  });
+});
 
 io.on('error', (err) => {
-  console.log('error with socket io:', err);
+  debug('error with socket io:', err);
 });
 
 
@@ -17,10 +36,25 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.send("Hello, Express!");
-});
+app.use(express.static(path.join(path.dirname(__dirname), 'public')));
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+
+
+app.set("port", port);
+const server = http.createServer(app);
+
+server.listen(port);
+server.on('listening', onListening);
+
+io.attach(server)
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
