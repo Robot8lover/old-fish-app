3f4dd460f048814630f98663e82de02256ca1b27
Robot8lover <108429990+Robot8lover@users.noreply.github.com>
2024-08-05 23:47:57 -0700

    Update game-script.js

--- public/javascripts/game-script.js
+++ public/javascripts/game-script.js
@@ -1,2 +1,24 @@
-const socket = io();
+const onLoad = () => {
+  const createGameBtn = document.getElementById("create-game");
+  const gameIdInput = document.getElementById("game-id");
+  const joinGameBtn = document.getElementById("join-game");
 
+  const socket = io();
+
+  createGameBtn.addEventListener("click", () => {
+    let maxPlayers = 0;
+    for (element of documents.getElementsByName("max-players")) {
+      if (element.checked) {
+        maxPlayers = +element.value;
+        break;
+      }
+    }
+    if (maxPlayers === 0) {
+      return;
+    }
+    socket.emit("game:create", maxPlayers);
+  }, false);
+  joinGameBtn.addEventListener("click", () => {}, false);
+};
+
+document.addEventListener("DOMContentLoaded", onLoad, false);
