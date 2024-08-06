88f252543095c0fdf64080122bbe2246e9a81cae
Robot8lover <108429990+Robot8lover@users.noreply.github.com>
2024-08-06 10:26:16 -0700

    Update game-script.js

--- public/javascripts/game-script.js
+++ public/javascripts/game-script.js
@@ -1,24 +1,58 @@
+"use strict";
+
+import ASK_DELAY from "./ASK_DELAY.js";
+
 const onLoad = () => {
-  const createGameBtn = document.getElementById("create-game");
-  const gameIdInput = document.getElementById("game-id");
-  const joinGameBtn = document.getElementById("join-game");
+  const joinPage = document.getElementById("join-page");
+  const gamePage = document.getElementById("game-page");
+  const createGameBtn = document.getElementById("create-btn");
+  const gameIdInput = document.getElementById("game-id-in");
+  const joinGameBtn = document.getElementById("join-btn");
 
   const socket = io();
 
-  createGameBtn.addEventListener("click", () => {
-    let maxPlayers = 0;
-    for (element of documents.getElementsByName("max-players")) {
-      if (element.checked) {
-        maxPlayers = +element.value;
-        break;
+  socket.on("game:join", (gameId) => {
+    joinPage.classList.add("hidden");
+    gamePage.classList.remove("hidden");
+    document.getElementById("game-id-span").textContent = gameId;
+  });
+
+  createGameBtn.addEventListener(
+    "click",
+    () => {
+      let maxPlayers = 0;
+      for (const element of document.getElementsByName("max-players")) {
+        if (element.checked) {
+          maxPlayers = +element.value;
+          break;
+        }
       }
-    }
-    if (maxPlayers === 0) {
-      return;
-    }
-    socket.emit("game:create", maxPlayers);
-  }, false);
-  joinGameBtn.addEventListener("click", () => {}, false);
+      if (maxPlayers === 0) {
+        return;
+      }
+      socket.emit("game:create", maxPlayers);
+    },
+    false
+  );
+
+  joinGameBtn.addEventListener(
+    "click",
+    () => {
+      socket.emit("game:join", gameIdInput.value);
+    },
+    false
+  );
+
+  document.getElementById("leave-btn").addEventListener(
+    "click",
+    () => {
+      socket.emit("game:leave");
+      joinPage.classList.remove("hidden");
+      gamePage.classList.add("hidden");
+      document.getElementById("game-id-span").textContent = "";
+    },
+    false
+  );
 };
 
 document.addEventListener("DOMContentLoaded", onLoad, false);
