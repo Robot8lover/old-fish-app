0755b08053cf6d96ae8b5936372234166f9e4f2d
Robot8lover <108429990+Robot8lover@users.noreply.github.com>
2024-08-07 19:35:47 -0700

    Update constants.js

--- public/shared_js/constants.js
+++ public/shared_js/constants.js
@@ -1,6 +1,6 @@
 // This file is probably a terrible idea.
 
 const ASK_DELAY = 1000 * 5; // Delay between asks (ms)
-const NAME_LEN = 64; // Max character length of a name
+const NAME_LEN = 16; // Max character length of a name
 
 export { ASK_DELAY, NAME_LEN };
